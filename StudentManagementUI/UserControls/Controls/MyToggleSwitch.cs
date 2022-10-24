a1758b40c6c79f9c8be4c2b09b8b6a50d838a36b
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-10-24 14:52:40 +0300

    ..

---
+++ StudentManagementUI/UserControls/Controls/MyToggleSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Utils;
+using DevExpress.XtraEditors;
+using StudentManagementUI.Abstract;
+
+namespace StudentManagementUI.UserControls.Controls
+{
+    [ToolboxItem(true)]
+    public class MyToggleSwitch:ToggleSwitch,IStatusBarDescription
+    {
+        public MyToggleSwitch()
+        {
+            Name = "tglState";
+            Properties.OnText = "Active";
+            Properties.OffText = "Passive";
+            Properties.AutoHeight = false;
+            Properties.AutoWidth = true;
+            IsOn = true;
+            Properties.GlyphAlignment = HorzAlignment.Far;
+            Properties.Appearance.ForeColor = Color.Maroon;
+        }
+
+        public override bool EnterMoveNextControl { get; set; } = true;
+        public string StatusBarDescription { get; set; } = "Choose the state of record";
+    }
+}
